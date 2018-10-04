7bcf2dc3bc67b208b62fec786f1004a152c9e021
GregoryDias29 <35336208+GregoryDias29@users.noreply.github.com>
2018-10-04 09:22:15 -0300

    Add files via upload

---
+++ Calcular_N_esimo_Elementos_Fibonacci.java
@@ -0,0 +1,24 @@
+package facu;
+
+/*22)Elaborar um programa, em linguagem Java, para calcular o N-ésimo elemento da
+série de Fibonacci. O índice desse elemento deverá ser digitado para a realização do
+cálculo. Criar um método que calcula o resultado da série, utilizando-se da
+recursividade.*/
+
+public class Calcular_N_esimo_Elementos_Fibonacci {
+	static long fibo(int n) {
+		if (n < 2) {
+			return n;
+		} else {
+			return fibo(n - 1) + fibo(n - 2);
+		}
+	}
+
+	public static void main(String[] args) {
+		// neste ponto vc pode alterar para até quantos numeros vc quiser que ele faça o
+		// fibonacci
+		for (int i = 0; i < 31; i++) {
+			System.out.print("(" + i + "):" + Calcular_N_esimo_Elementos_Fibonacci.fibo(i) + "\t");
+		}
+	}
+}
