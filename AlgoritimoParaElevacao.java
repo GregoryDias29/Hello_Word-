b7c3e9e86492df44158e0bc971e45dfa65972f3c
JPAS1X <anunciacao.jpedro@gmail.com>
2019-08-23 21:51:34 -0300

    Update AlgoritimoParaElevacao.java

--- AlgoritimoParaElevacao.java
+++ AlgoritimoParaElevacao.java
@@ -1,13 +1,13 @@
 import javax.swing.JOptionPane;
 public class AlgoritimoParaElevacao {
    public static void main(String[] args){
-      String n = JOptionPane.showInputDialog("Digite o valor á ser elevado ");
+      String n = JOptionPane.showInputDialog("Digite o valor Ã¡ ser elevado 2 ");
      
      double valor = Double.parseDouble (n);   
      double b = Math.pow(valor,2);
      
       
  
-      JOptionPane.showMessageDialog(null,"o valor é "+b);
+      JOptionPane.showMessageDialog(null,"o valor Ã© "+b);
       }
 }
